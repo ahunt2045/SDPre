721417e307e98a767750b214527171bf413dd981
Andrew <ahunt2045@gmail.com>
2020-08-06 18:51:46 -0600

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int totalSum = 0;
+    for (int i = 1; i <=20; i++) {
+      totalSum += i;
+    }
+    System.out.println(totalSum);
+  }
+}
